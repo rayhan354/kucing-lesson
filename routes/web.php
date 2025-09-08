fdd91d25fe190788d66abb31566339274e25f960
Rayhan <rayhan354@gmail.com>
2025-09-08 13:36:19 +0700

    new file:   app/Http/Controllers/CourseController.php 	new file:   app/Http/Controllers/DashboardController.php 	new file:   app/Http/Controllers/FrontController.php 	modified:   resources/views/welcome.blade.php 	modified:   routes/web.php 	new file:   routes/web.php.bak 	modified:   storage/framework/views/283fd1d3c1cd62041fd6f14ba46ad351.php 	new file:   storage/framework/views/4f6a71d11c943702133ac06f4f861590.php 	modified:   storage/logs/laravel.log

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,64 @@
 <?php
 
-use App\Livewire\Settings\Appearance;
-use App\Livewire\Settings\Password;
-use App\Livewire\Settings\Profile;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+Route::get('/pricing', [FrontController::class, 'pricing'])->name('front.pricing');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::match(['get', 'post'], '/booking/payment/midtrans/notification',
+[FrontController::class, 'paymentMidtransNotification'])
+    ->name('front.payment_midtrans_notification');
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    Route::middleware('role:student')->group(function () {
+        Route::get('/dashboard/subscriptions/', [DashboardController::class, 'subscriptions'])
+        ->name('dashboard.subscriptions');
+
+        // model binding 1, 23, 412 ,2121
+        Route::get('/dashboard/subscription/{transaction}', [DashboardController::class, 'subscription_details'])
+        ->name('dashboard.subscription.details');
+
+        Route::get('/dashboard/courses/', [CourseController::class, 'index'])
+        ->name('dashboard');
+
+        // slug web-design
+        Route::get('/dashboard/course/{course:slug}', [CourseController::class, 'details'])
+        ->name('dashboard.course.details');
+
+        Route::get('/dashboard/search/courses', [CourseController::class, 'search_courses'])
+        ->name('dashboard.search.courses');
+
+        Route::middleware(['check.subscription'])->group(function () {
+            Route::get('/dashboard/join/{course:slug}', [CourseController::class, 'join'])
+            ->name('dashboard.course.join');
+
+            // web-design-hack/1/12
+            Route::get('/dashboard/learning/{course:slug}/{courseSection}/{sectionContent}', [CourseController::class, 'learning'])
+            ->name('dashboard.course.learning');
+
+            Route::get('/dashboard/learning/{course:slug}/finished', [CourseController::class, 'learning_finished'])
+            ->name('dashboard.course.learning.finished');
+        });
+
+        Route::get('/checkout/success', [FrontController::class, 'checkout_success'])
+        ->name('front.checkout.success');
+
+        Route::get('/checkout/{pricing}', [FrontController::class, 'checkout'])
+        ->name('front.checkout');
+
+        Route::post('/booking/payment/midtrans', [FrontController::class, 'paymentStoreMidtrans'])
+        ->name('front.payment_store_midtrans');
+    });
+
+
 });
 
 require __DIR__.'/auth.php';
